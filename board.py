8b3d853818eca2197c08f14aefab7b2db2b8b7a2
prac88 <125519816+prac88@users.noreply.github.com>
2023-02-15 19:21:15 -0600

    Update board.py

--- board.py
+++ board.py
@@ -5,22 +5,22 @@ Class Board:
     import numpy as np
     pygame.init()
 
-    WIDTH = 600
-    HEIGHT = 600
-    LINE_WIDTH = 15
-    WIN_LINE_WIDTH = 15
-    BOARD_ROWS = 3
-    BOARD_COLS = 3
-    SQUARE_SIZE = 200
-    CIRCLE_RADIUS = 60
-    CIRCLE_WIDTH = 15
-    CROSS_WIDTH = 25
-    SPACE = 55
+    Global WIDTH = 600
+    Global HEIGHT = 600
+    Global LINE_WIDTH = 15
+    Global WIN_LINE_WIDTH = 15
+    Global BOARD_ROWS = 3
+    Global BOARD_COLS = 3
+    Global SQUARE_SIZE = 200
+    Global CIRCLE_RADIUS = 60
+    Global CIRCLE_WIDTH = 15
+    Global CROSS_WIDTH = 25
+    Global SPACE = 55
 
-    BG_COLOR = (20, 200, 160)
-    LINE_COLOR = (23, 145, 135)
-    CIRCLE_COLOR = (239, 231, 200)
-    CROSS_COLOR = (66, 66, 66)
+    Global BG_COLOR = (20, 200, 160)
+    Global LINE_COLOR = (23, 145, 135)
+    Global CIRCLE_COLOR = (239, 231, 200)
+    Global CROSS_COLOR = (66, 66, 66)
 
     screen = pygame.display.set_mode( (WIDTH, HEIGHT) )
     pygame.display.set_caption( 'TIC TAC TOE' )

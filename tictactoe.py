647e0bac27c4b82239286325c568caeb8042e0e5
jiyeongchloe <109016421+jiyeongchloe@users.noreply.github.com>
2023-02-15 20:25:27 -0600

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,14 @@
 """
 tic tac toe
 """
+class TicTacToe:
+  def __init__(self):
+    
+  def check_winner():
+    """
+    check if there is winner
+    returns player1, player2, or none
+    """
+    raise NotImplementedError
+
+  def 
